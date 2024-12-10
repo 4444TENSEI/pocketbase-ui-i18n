2f9fd293e1c931bc1c73f6c5f4ff2b7f41762ce1
夜風 <2479757568@qq.com>
2024-12-11 05:01:43 +0800

    First release

---
+++ src/stores/app.js
@@ -0,0 +1,13 @@
+//sotres/app.js
+import { writable } from "svelte/store";
+import { getCookie } from '@/utils/Cookie';
+
+export const pageTitle = writable('');
+
+export const appName = writable('');
+
+export const hideControls = writable(false);
+
+const envAppUrl = writable(import.meta.env.PB_BACKEND_URL);
+// 🐱
+const backendUrl = getCookie('appUrl') || envAppUrl;
